3ca6931e932157be73ff10f6cf423ffe75f58539
Qurull <Qurull@users.noreply.github.com>
2026-09-10 21:51:40 +0200

    #

--- GameGroupProject/Program.cs
+++ GameGroupProject/Program.cs
@@ -4,89 +4,177 @@ namespace GameGroupProject
 {
     internal class Program
     {
-        static readonly string[] games = { "Hangman", "MineSweeper", "Jeopardy", "MasterMind" };
+        static readonly Random random = new();
+
+        static readonly string[] gameItems = ["Hangman", "MineSweeper", "Jeopardy", "MasterMind"];
+
+        static readonly ConsoleColor[] colors = [ConsoleColor.Red, ConsoleColor.Green, ConsoleColor.Blue, ConsoleColor.Cyan, ConsoleColor.Magenta, ConsoleColor.Yellow];
+
+        static readonly ConsoleColor[] darkColors = [ConsoleColor.DarkRed, ConsoleColor.DarkGreen, ConsoleColor.DarkBlue, ConsoleColor.DarkCyan, ConsoleColor.DarkMagenta, ConsoleColor.DarkYellow, ConsoleColor.DarkGray];
+       
+        static readonly char[] chars = ['●', '○', '◆', '◈', '◇', '×', '◻', '◼'];
 
         static void Main(string[] args)
         {
-            Console.InputEncoding = Encoding.UTF8;
             Console.OutputEncoding = Encoding.UTF8;
-            DisplayMainMenu();
+            PrintMainMenu();
+            Console.ReadKey(true);
         }
 
-        /// <summary>
-        /// Displays the title and the game selection menu,
-        /// then displays a loading message for the selected game.
-        /// </summary>
-        static void DisplayMainMenu()
+        static void PrintMainMenu()
         {
-            WriteGameTitle();
-
-            int selectedGame = SelectGame();
-            Console.WriteLine($"Loading {games[selectedGame]} now...");
-        }
-
-        /// <summary>
-        /// Displays the title in the console.
-        /// </summary>
-        static void WriteGameTitle()
-        {
-            string title = "◈--- Welcome to Arcade! ---◈";
-            for (int i = 0; i < title.Length; i++)
-            {
-                Console.ForegroundColor = i % 2 == 0 ? ConsoleColor.Yellow : ConsoleColor.Cyan;
-                Console.Write(title[i]);
-            }
-            Console.WriteLine();
-            Console.ResetColor();
-        }
-
-        /// <summary>
-        /// Displays the game selection menu and allows the user to navigate
-        /// through the available games using the Up and Down arrow keys.
-        /// Pressing Enter selects the currently highlighted game.
-        /// </summary>
-        /// <returns>The index of the selected game in the games array.</returns>
-        static int SelectGame()
-        {
-            (int left, int top) = Console.GetCursorPosition();
+            Console.Title = "Arcade";
             Console.CursorVisible = false;
 
-            int selectedIndex = 0;
+            PrintBackground();
+            PrintGameTitle("Welcome to Arcade!", (Console.WindowWidth/2 - 24, 3), (16, 7));
+            int selectedGame = PrintGameMenu(0, (0, Console.WindowHeight/2));
+            PrintGameLoading($"Loading {gameItems[selectedGame]}");
+            
+            if (selectedGame == 0)
+                return;
+            else if (selectedGame == 1)
+                return;
+            else if (selectedGame == 2)
+                return;
+            else if (selectedGame == 3)
+                return;
+        }
 
-            while (true)
+        static void PrintBackground()
+        {
+            for (int y = 0; y < Console.WindowHeight; y++)
             {
-                WriteGameMenu(selectedIndex, left, top);
-
-                switch (Console.ReadKey().Key)
+                for (int x = 0; x < Console.WindowWidth; x++)
                 {
-                    case ConsoleKey.UpArrow:
-                        selectedIndex = Math.Max(selectedIndex - 1, 0);
-                        break;
-                    case ConsoleKey.DownArrow:
-                        selectedIndex = Math.Min(selectedIndex + 1, games.Length - 1);
-                        break;
-                    case ConsoleKey.Enter:
-                        return selectedIndex;
+                    if (random.NextDouble() > 0.05) 
+                        continue;
+                    Console.ForegroundColor = darkColors[random.Next(darkColors.Length)];
+                    Console.SetCursorPosition(x, y);
+                    Console.Write(chars[random.Next(chars.Length)]);
+                    Console.ResetColor();
                 }
             }
         }
 
-        /// <summary>
-        /// Displays the game selection menu and highlights the currently selected game.
-        /// </summary>
-        /// <param name="selectedIndex">The index of the currently selected game.</param>
-        /// <param name="left">Column position of the cursor, where the menu should start.</param>
-        /// <param name="top">Row position of the cursor, where the menu should start.</param>
-        static void WriteGameMenu(int selectedIndex, int left, int top)
+        static void PrintGameTitle(string text, (int x, int y) position, (int x, int y) size)
         {
-            Console.SetCursorPosition(left, top);
-            for (int i = 0; i < games.Length; i++)
+            for (int y = 0; y <= size.y; y++)
             {
-                bool selected = selectedIndex == i;
-                Console.ForegroundColor = selected ? ConsoleColor.Yellow : ConsoleColor.White;
-                Console.WriteLine($"{(selected ? "◉" : "○")} {games[i]}");
+                for (int x = 0; x <= size.x; x++)
+                {
+                    char character = ' ';
+                    if (x == 0 || x == size.x || y == 0 || y == size.y)
+                        character = chars[random.Next(chars.Length)];
+
+                    Console.ForegroundColor = colors[random.Next(colors.Length)];
+                    Console.SetCursorPosition(position.x + x * 3, position.y + y);
+                    Console.Write(character + "  ");
+                    Console.ResetColor();
+                }
+                Thread.Sleep(100);
+            }
+            Console.ForegroundColor = ConsoleColor.White;
+            for (int i = 0; i < text.Length; i++)
+            {
+                Console.SetCursorPosition(position.x + (size.x * 2 - text.Length)/2 + i*2, position.y + size.y/2 + i % 2);
+                Console.Write(text[i]);
+                Thread.Sleep(50);
             }
             Console.ResetColor();
+        }
+
+        static int PrintGameMenu(int index, (int x, int y) position)
+        {
+            const int cardWidth = 16;
+            const int spacing = 2;
+
+            int totalWidth = gameItems.Length * cardWidth + (gameItems.Length - 1) * spacing;
+            int startX = (Console.WindowWidth - totalWidth) / 2;
+            int selectedItem = index;
+
+            for (int i = 0; i < gameItems.Length; i++)
+                PrintGameItem(i, selectedItem == i);
+
+            while (true)
+            {
+                int previousItem = selectedItem;
+                ConsoleKeyInfo keyInfo = Console.ReadKey(true);
+
+                if (keyInfo.Key == ConsoleKey.LeftArrow)
+                    selectedItem = selectedItem > 0 ? selectedItem - 1 : gameItems.Length - 1;
+                else if (keyInfo.Key == ConsoleKey.RightArrow)
+                    selectedItem = (selectedItem + 1) % gameItems.Length;
+                else if (keyInfo.Key == ConsoleKey.Enter)
+                    break;
+
+                PrintGameItem(previousItem, false);
+                PrintGameItem(selectedItem, true);
+            }
+
+            void PrintGameItem(int index, bool selected)
+            {
+                string item = gameItems[index];
+                int x = startX + index * (cardWidth + spacing);
+
+                ConsoleColor color = selected ? colors[random.Next(colors.Length - 1)] : ConsoleColor.DarkGray;
+                (int x, int y, int width, int height) gameItem = PrintBorder(null, color, color, (position.x + x, position.y + index % 2), (cardWidth, 4));
+                
+                Console.SetCursorPosition(x + (gameItem.width - item.Length) / 2 + 1, gameItem.y + gameItem.height / 2);
+                Console.ForegroundColor = color;
+                Console.WriteLine(item);
+                Console.ResetColor();
+            }
+
+            return selectedItem;
+        }
+
+        static void PrintGameLoading(string text)
+        {
+            Console.Clear();
+            PrintBackground();
+            for (int i = 0; i < 12; i++)
+            {
+                Console.SetCursorPosition((Console.WindowWidth - text.Length - 3)/2, Console.WindowHeight/2);
+                Console.Write($"{text}{new('.', i % 4)}   ");
+                Thread.Sleep(200);
+            }
+        }
+
+        static (int x, int y, int width, int height) PrintBorder(string? title, ConsoleColor titleColor, ConsoleColor borderColor, (int x, int y) position, (int x, int y) size, bool particles = false, bool animate = false)
+        {
+            for (int y = 0; y <= size.y; y++)
+            {
+                for (int x = 0; x <= size.x; x++)
+                {
+                    char character = ' ';
+                    if (x == 0 && y == 0) character = '┌';
+                    else if (x == size.x && y == 0) character = '┐';
+                    else if (x == 0 && y == size.y) character = '└';
+                    else if (x == size.x && y == size.y) character = '┘';
+                    else if (y == 0 || y == size.y) character = '─';
+                    else if (x == 0 || x == size.x) character = '│';
+
+                    if (particles && random.NextDouble() <= 0.1 && x > 0 && x < size.x && y > 0 && y < size.y)
+                    {
+                        Console.ForegroundColor = ConsoleColor.DarkGray;
+                        character = '•';
+                    }
+                    Console.ForegroundColor = borderColor;
+                    Console.SetCursorPosition(position.x + x, position.y + y);
+                    Console.Write(character);
+                }
+                if (animate) Thread.Sleep(50);
+            }
+            if (title != null)
+            {
+                Console.ForegroundColor = titleColor;
+                Console.SetCursorPosition(position.x + (size.x - (title.Length + 2))/2, position.y);
+                Console.Write($" {title} ");
+            }
+            Console.ResetColor();
+
+            return (position.x, position.y, size.x, size.y);
         }
     }
 }
